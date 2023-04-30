68697b96dd4ca781bf74ed4d03af6a0aa16a6ea6
Ngoc thinh nguyen <nnthinh88@gmail.com>
2023-04-30 01:08:19 -0700

    add new bill button using rest state

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,72 +4,77 @@ import {DISHES} from '../shares/dishes';
 import Header from './HeaderComponent';
 import Receipt from './ReceiptComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import NewBill from './NewBillComponent';
+import { Button } from 'reactstrap';
 
+const initalState = {
+    dishes: DISHES,
+    selectedDishes: []
+};
 class Main extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDishes: []};
+        this.state = initalState;
     }
     onDishSelect =(dish)=>{
-        console.log('here');
         this.setState({
             selectedDishes: [...this.state.selectedDishes, dish],
         })
     }
+    resetDish =()=>{
+        this.setState( initalState);
+    }
 
     render(){
-        console.log(this.state.selectedDishes);
-        console.log(this.state.totalPrice);
-
         const appertizerPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='appertizer')} onClick={(dish)=>this.onDishSelect(dish)}  />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='appertizer')} onClick={(dish)=>this.onDishSelect(dish)}  />
             )
         }
         const newDishPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='new dish')} onClick={(dish)=>this.onDishSelect(dish)} />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='new dish')} onClick={(dish)=>this.onDishSelect(dish)} />
             )
         }
         const phoPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='pho')} onClick={(dish)=>this.onDishSelect(dish)} />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='pho')} onClick={(dish)=>this.onDishSelect(dish)} />
             )
         }
         const bunPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='bun')} onClick={(dish)=>this.onDishSelect(dish)} />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='bun')} onClick={(dish)=>this.onDishSelect(dish)} />
             )
         }
         const vermicelliPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='vermicelli')} onClick={(dish)=>this.onDishSelect(dish)} />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='vermicelli')} onClick={(dish)=>this.onDishSelect(dish)} />
             )
         }
         const comPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='com')} onClick={(dish)=>this.onDishSelect(dish)} />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='com')} onClick={(dish)=>this.onDishSelect(dish)} />
             )
         }
         const drinkPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='drink')} onClick={(dish)=>this.onDishSelect(dish)} />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='drink')} onClick={(dish)=>this.onDishSelect(dish)} />
             )
         }
         const sideOrderPage = () => {
             return(
-                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category =='side order')} onClick={(dish)=>this.onDishSelect(dish)} />
+                <RenderDish dishes={this.state.dishes.filter((dish)=>dish.category ==='side order')} onClick={(dish)=>this.onDishSelect(dish)} />
             )
         }
         
         return(
             <div>
-                <NewBill />
+                <div className='container mt-4 mb-4 '>
+                    <div className='row justify-content-center'>
+                        <Button color='primary' onClick={this.resetDish}>New Bill</Button>
+                    </div>
+                </div>
                 <Receipt receipt={this.state.selectedDishes}/>
-                <Header />
+                <Header onClickHandler={this.resetDish} />
                 <Switch>
                     <Route path='/appertizer' component={appertizerPage} />
                     <Route path='/newDish' component={newDishPage} />
--- src/components/ReceiptComponent.js
+++ src/components/ReceiptComponent.js
@@ -2,52 +2,78 @@ import React from 'react';
 import { Table } from 'reactstrap';
 
 const Receipt = (props) => {
-    const receipt = props.receipt.map((item)=>{
+    if(props.receipt.length !== 0){
+        const receipt = props.receipt.map((item)=>{
+            return(
+                    <tr>
+                        <th>{item.id}</th>
+                        <th>{item.name}</th>
+                        <th>{item.price}</th>
+                    </tr>
+                )
+        });
+        const total = props.receipt.map(item=>item.price).reduce((a,b)=>a+b,0);
+
         return(
-                <tr>
-                    <th>{item.id}</th>
-                    <th>{item.name}</th>
-                    <th>{item.price}</th>
-                </tr>
-            )
-    });
-    const total = props.receipt.map(item=>item.price).reduce((a,b)=>a+b,0);
+            <div className='container table-scrolling'>
+                <div className='row '>
+                    <Table borderless hover responsive>
+                    <thead>
+                            <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Price
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            {receipt}
+                        </tbody>
+                    </Table>
+                    <Table>
+                        <tbody>
+                            <tr>
+                                <th><a style={{color:'red', fontSize:'30px'}}>Total</a></th>
+                                <th></th>
+                                <th><a style={{color:'red', fontSize:'30px'}}>{total.toFixed(2)}</a></th>
+                            </tr>
+                        </tbody>
+                    </Table>
 
-    return(
-        <div className='container table-scrolling'>
-            <div className='row '>
-                <Table borderless hover responsive>
-                <thead>
-                        <tr>
-                        <th>
-                            #
-                        </th>
-                        <th>
-                            Name
-                        </th>
-                        <th>
-                            Price
-                        </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                        {receipt}
-                    </tbody>
-                </Table>
-                <Table>
-                    <tbody>
-                        <tr>
-                            <th><a style={{color:'red', fontSize:'30px'}}>Total</a></th>
-                            <th></th>
-                            <th><a style={{color:'red', fontSize:'30px'}}>{total.toFixed(2)}</a></th>
-                        </tr>
-                    </tbody>
-                </Table>
-
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    else{
+        return(
+            <div className='container table-scrolling'>
+                <div className='row '>
+                    <Table borderless hover responsive>
+                    <thead>
+                            <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Price
+                            </th>
+                            </tr>
+                        </thead>
+                    </Table>
+    
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Receipt;
