ff595fae2407bf392273115237c1d4f74c53e257
Ngoc thinh nguyen <nnthinh88@gmail.com>
2023-05-05 01:51:50 -0700

    change some to new price

--- src/components/RenderComponent.js
+++ src/components/RenderComponent.js
@@ -4,9 +4,9 @@ import { Card, CardTitle,Nav,NavItem,NavLink } from 'reactstrap';
 const RenderDish = (props) => {
     const renderDish = props.dishes.map((dish)=>{
         return(
-                <div  className="col-2 col-md-3 p-1" >
+                <div  className="col-2 col-md-3 p-1 mb-2" >
                     <Card onClick={()=>props.onClick(dish)} color="primary" outline="true" key={dish.id}>
-                        <CardTitle style={{fontSize: '15px'}} color="white">{dish.id}</CardTitle>
+                        <CardTitle style={{fontSize: '18px'}} color="white">{dish.id}</CardTitle>
                     </Card>
                 </div>
             )
